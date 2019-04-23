04f07b6e2319ba02945d0941d61a452a19c1400d
yukms <763803382@qq.com>
2019-04-23 10:46:15 +0800

    swagger：结构调整

--- src/test/java/com/yukms/learn/swagger/AppTest.java
+++ src/test/java/com/yukms/learn/swagger/AppTest.java
@@ -1,7 +1,6 @@
 package com.yukms.learn.swagger;
 
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 /**
---
+++ swagger/src/main/java/com/yukms/learn/swagger/controller/SystemService.java
@@ -0,0 +1,9 @@
+package com.yukms.learn.swagger.controller;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * yukms 763803382@qq.com 2019/3/20 18:24
+ */
+@Service("systemService")
+public class SystemService { }
