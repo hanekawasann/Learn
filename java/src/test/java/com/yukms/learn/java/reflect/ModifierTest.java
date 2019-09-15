10427ace33e2d5832ca7543bd0220d1d9a9dfff4
yukms <763803382@qq.com>
2019-09-15 18:51:41 +0800

    java

---
+++ java/src/test/java/com/yukms/learn/java/reflect/ModifierTest.java
@@ -0,0 +1,15 @@
+package com.yukms.learn.java.reflect;
+
+import java.lang.reflect.Modifier;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ModifierTest {
+    @Test
+    public void test_isPublic() {
+        Assert.assertTrue(Modifier.isPublic(1));
+        Assert.assertFalse(Modifier.isPublic(10));
+        Assert.assertTrue(Modifier.isPublic(11));
+    }
+}
