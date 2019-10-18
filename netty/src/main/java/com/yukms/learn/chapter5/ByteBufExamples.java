9b877e8d2172e7c66929a425ce8d892cb695c7d4
yukms <763803382@qq.com>
2019-10-18 10:16:28 +0800

    netty

--- netty/src/main/java/com/yukms/learn/chapter5/ByteBufExamples.java
+++ netty/src/main/java/com/yukms/learn/chapter5/ByteBufExamples.java
@@ -1,20 +1,13 @@
 package com.yukms.learn.chapter5;
 
 import java.nio.ByteBuffer;
-import java.util.Random;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.CompositeByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class ByteBufExamples {
-    private final static Random random = new Random();
     private static final ByteBuf BYTE_BUF_FROM_SOMEWHERE = Unpooled.buffer(1024);
-    private static final Channel CHANNEL_FROM_SOMEWHERE = new NioSocketChannel();
-    private static final ChannelHandlerContext CHANNEL_HANDLER_CONTEXT_FROM_SOMEWHERE = null;
 
     /** 堆缓冲区 */
     public static void heapBuffer() {
