ec90fa2e7e09541cca419dee14d8a99640c597fc
yukms <763803382@qq.com>
2019-12-18 19:16:00 +0800

    不想写注释

--- java/src/test/java/com/yukms/learn/java/util/stream/StreamTest.java
+++ java/src/test/java/com/yukms/learn/java/util/stream/StreamTest.java
@@ -2,27 +2,29 @@ package com.yukms.learn.java.util.stream;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.Test;
 
 /**
  * @author hudingpeng hudingpeng@souche.com 2019/11/18 13:55
  */
 public class StreamTest {
-    @Test
-    public void test_01() {
-        List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        list.add("4");
-        list.add(null);
-        System.out.println(list);
-        System.out.println(list.stream().filter(Objects::nonNull).collect(Collectors.toList()));
-        System.out.println(list);
-        list.removeIf(Objects::isNull);
-        System.out.println(list);
+
+    @AllArgsConstructor
+    @Data
+    private static class People {
+        private String name;
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_toMap() {
+        List<People> list = new ArrayList<>();
+        list.add(new People("1"));
+        list.add(new People("1"));
+        list.stream().collect(Collectors.toMap(People::getName, Function.identity()));
     }
 }
