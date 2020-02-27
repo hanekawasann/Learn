26968cbdca07e1647bc624f7655acbd99048b20b
yukms <763803382@qq.com>
2020-02-27 17:21:33 +0800

    不想写注释

--- java/src/test/java/com/yukms/learn/java/time/DurationTest.java
+++ java/src/test/java/com/yukms/learn/java/time/DurationTest.java
@@ -1,6 +1,20 @@
 package com.yukms.learn.java.time;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import org.junit.Test;
+
 /**
  * @author hudingpeng hudingpeng@souche.com 2020/2/25 9:56
  */
-public class DurationTest { }
+public class DurationTest {
+    @Test
+    public void test_() {
+        LocalDateTime start = LocalDateTime.now();
+        LocalDateTime end = LocalDateTime.now();
+        Duration duration = Duration.between(start, end);
+        System.out.println(duration.get(ChronoUnit.SECONDS));
+    }
+}
